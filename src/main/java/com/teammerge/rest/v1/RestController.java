a8f62a9d158085311def33252ae80249a0910412
kishore nammi <rahul.bokolia@reflexsolution.com>
2017-07-05 12:20:04 +0530

    resolving Ticket-1 changes

--- src/main/java/com/teammerge/rest/v1/RestController.java
+++ src/main/java/com/teammerge/rest/v1/RestController.java
@@ -1,12 +1,8 @@
 package com.teammerge.rest.v1;
 
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.annotation.Resource;
 import javax.ws.rs.DefaultValue;
@@ -20,17 +16,12 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.google.common.net.MediaType;
-import com.teammerge.manager.IRepositoryManager;
 import com.teammerge.model.ActivityModel;
-import com.teammerge.model.ExtCommitModel;
-import com.teammerge.model.TicketCommitsModel;
 import com.teammerge.model.RefModel;
 import com.teammerge.services.DashBoardService;
-import com.teammerge.services.CommitService;
 import com.teammerge.services.RepositoryService;
 import com.teammerge.utils.JGitUtils;
 import com.teammerge.utils.JacksonUtils;
@@ -39,196 +30,173 @@ import com.teammerge.utils.JacksonUtils;
 @Path("/v1")
 public class RestController {
 
-	private static String finalOutput = null;
+  @Resource(name = "repositoryService")
+  private RepositoryService repositoryService;
 
-	@Resource(name = "repositoryService")
-	private RepositoryService repositoryService;
+  @Resource(name = "dashBoardService")
+  private DashBoardService dashBoardService;
 
-	@Resource(name = "dashBoardService")
-	private DashBoardService dashBoardService;
+  @Value("${app.debug}")
+  private String debug;
 
-	@Resource(name = "commitService")
-	private CommitService commitService;
+  public boolean isDebugOn() {
+    return Boolean.parseBoolean(debug);
+  }
 
-	private IRepositoryManager getRepositoryManager() {
-		return repositoryService.getRepositoryManager();
-	}
+  @GET
+  @Path("/")
+  public Response hello() {
+    applicationPaths();
+    return Response.status(200).entity("hi rfsdal").build();
+  }
 
-	@GET
-	@Path("/")
-	public Response hello() {
-		return Response.status(200).entity("hi rfsdal").build();
-	}
+  @GET
+  @Path("/hello/{param}")
+  public Response getMsg(@PathParam("param") String msg) {
+    String output = "Jersey say : " + msg;
+    return Response.status(200).entity(output).build();
+  }
 
-	@GET
-	@Path("/hello/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
-		String output = "Jersey say : " + msg;
-		return Response.status(200).entity(output).build();
-	}
+  @GET
+  @Path("/dataTable")
+  public Response sampleDataTableExample() {
+    String output = "";
 
-	@GET
-	@Path("/dataTable")
-	public Response sampleDataTableExample() {
-		String output = "";
+    output += "{";
+    output += "\"data\": [";
+    output += "{";
+    output += "\"name\": \"Tiger Nixon\",";
+    output += "\"position\": \"System Architect\",";
+    output += "\"salary\": \"$320,800\",";
+    output += "\"start_date\": \"2011/04/25\",";
+    output += "\"office\": \"Edinburgh\",";
+    output += "\"extn\": \"5421\"";
+    output += "}]}";
+    return Response.status(200).entity(output).header("Access-Control-Allow-Origin", "*").build();
+  }
 
-		output += "{";
+  @GET
+  @Path("/repositories")
+  public Response getRepositoriesName() {
+    List<String> list = repositoryService.getRepositoryList();
+    System.out.println("\n\n " + list.size() + "\n\n");
+    String output = "";
 
-		output += "\"data\": [";
-		output += "{";
-		output += "\"name\": \"Tiger Nixon\",";
-		output += "\"position\": \"System Architect\",";
-		output += "\"salary\": \"$320,800\",";
-		output += "\"start_date\": \"2011/04/25\",";
-		output += "\"office\": \"Edinburgh\",";
-		output += "\"extn\": \"5421\"";
-		output += "}]}";
-		return Response.status(200).entity(output).header("Access-Control-Allow-Origin", "*").build();
-	}
+    for (String list1 : list) {
+      System.out.println("\n\n " + list1 + "\n\n");
+      output += list1 + "<br>";
+    }
+    return Response.status(200).entity(output).header("Access-Control-Allow-Origin", "*").build();
+  }
 
-	@GET
-	@Path("/repositories")
-	public Response getRepositoriesName() {
-		List<String> list = getRepositoryManager().getRepositoryList();
-		System.out.println("\n\n " + list.size() + "\n\n");
-		String output = "";
+  @GET
+  @Path("/repository/gitlist")
+  public Response getRepoForBrowser() {
+    Repository repo = repositoryService.getRepository("gitlist", true);
+    String output = null;
+    if (repo == null) {
+      output = "Error in loading repository 'Gitlist'";
+      return Response.status(200).entity(output).build();
+    }
+    output = "Repository 'gitlist' successfully loaded!!";
+    System.out.println("Repo: " + repo);
+    return Response.status(200).entity(output).build();
+  }
 
-		for (String list1 : list) {
-			System.out.println("\n\n " + list1 + "\n\n");
-			output += list1 + "<br>";
-		}
+  @GET
+  @Path("/{repository}/commit")
+  public Response getCommit(@PathParam("repository") String repoName) {
+    String output = null;
+    Repository repo = repositoryService.getRepository(repoName, true);
 
-		return Response.status(200).entity(output).header("Access-Control-Allow-Origin", "*").build();
-	}
+    RevCommit commit = JGitUtils.getCommit(repo, null);
 
-	@GET
-	@Path("/repository/gitlist")
-	public Response getRepoForBrowser() {
-		Repository repo = getRepository("gitlist");
-		String output = null;
-		if (repo == null) {
-			output = "Error in loading repository 'Gitlist'";
-			return Response.status(200).entity(output).build();
-		}
-		output = "Repository 'gitlist' successfully loaded!!";
-		System.out.println("Repo: " + repo);
-		return Response.status(200).entity(output).build();
-	}
+    if (commit != null) {
+      Date commitDate = JGitUtils.getCommitDate(commit);
+      System.out.println("Commit: " + commit);
+      System.out.println("Message: " + commit.getFullMessage());
+      System.out.println("Name:" + commit.getName());
+      output = "Commit Date: " + commitDate;
+    } else {
+      output = "Unable to find commit!!";
+    }
+    return Response.status(200).entity(output).build();
+  }
 
-	@GET
-	@Path("/{repository}/commit")
-	public Response getCommit(@PathParam("repository") String repoName) {
-		String output = null;
-		Repository repo = getRepository(repoName);
+  @GET
+  @Path("/{repository}/commit/{branch}")
+  public Response getAllCommits(@PathParam("repository") String repoName,
+      @PathParam("branch") String branch) {
+    Repository repo = repositoryService.getRepository(repoName, true);
 
-		RevCommit commit = JGitUtils.getCommit(repo, null);
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(new Date(0));
+    System.out.println("Date = " + cal.getTime());
 
-		if (commit != null) {
-			Date commitDate = JGitUtils.getCommitDate(commit);
-			System.out.println("Commit: " + commit);
-			System.out.println("Message: " + commit.getFullMessage());
-			System.out.println("Name:" + commit.getName());
-			output = "Commit Date: " + commitDate;
-		} else {
-			output = "Unable to find commit!!";
-		}
-		return Response.status(200).entity(output).build();
-	}
+    List<RevCommit> commits = JGitUtils.getRevLog(repo, branch, cal.getTime());
+    String output = "";
+    if (CollectionUtils.isNotEmpty(commits)) {
+      for (RevCommit commit : commits) {
+        output += commit.getFullMessage() + "<br>";
+      }
+    } else {
+      output = "No commits found for the branch: " + branch;
+    }
+    return Response.status(200).entity(output).build();
+  }
 
-	@GET
-	@Path("/{repository}/commit/{branch}")
-	public Response getAllCommits(@PathParam("repository") String repoName, @PathParam("branch") String branch) {
-		Repository repo = getRepository(repoName);
+  @GET
+  @Path("/{repository}/branches")
+  public Response getAllBranches(@PathParam("repository") String repoName) {
+    ObjectId object = null;
+    String output = "";
 
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DATE, -20);
-		System.out.println("Date = " + cal.getTime());
+    Repository repo = repositoryService.getRepository(repoName, true);
+    List<RefModel> branchModels = JGitUtils.getRemoteBranches(repo, true, -1);
+    output += "Branches found: " + branchModels.size() + "<br><br>";
 
-		List<RevCommit> commits = JGitUtils.getRevLog(repo, branch, cal.getTime());
-		String output = "";
-		if (CollectionUtils.isNotEmpty(commits)) {
-			for (RevCommit commit : commits) {
-				output += commit.getFullMessage() + "<br>";
-			}
-		} else {
-			output = "No commits found for the branch: " + branch;
-		}
-		return Response.status(200).entity(output).build();
-	}
+    if (branchModels.size() > 0) {
 
-	@GET
-	@Path("/{repository}/branches")
-	public Response getAllBranches(@PathParam("repository") String repoName) {
-		ObjectId object = null;
-		String output = "";
+      for (RefModel branch : branchModels) {
+        object = branch.getReferencedObjectId();
+        output +=
+            branch.getName() + "--" + "Referenced Object Id: " + object + ", Object Id:"
+                + branch.getObjectId() + "<br>";
+      }
+    }
 
-		Repository repo = getRepository(repoName);
-		List<RefModel> branchModels = JGitUtils.getRemoteBranches(repo, true, -1);
-		output += "Branches found: " + branchModels.size() + "<br><br>";
-		Set<RefModel> uniqueSet = new HashSet<RefModel>(branchModels);
-		if (branchModels.size() > 0) {
+    return Response.status(200).entity(output).build();
 
-			for (RefModel branch : uniqueSet) {
-				object = branch.getReferencedObjectId();
-				String s1 = branch.getName();
-				String temp = s1.substring(20);
-				int number = Collections.frequency(branchModels, branch);
-				Calendar cal = Calendar.getInstance();
-				cal.add(Calendar.DATE, -20);
-				int count = 0;
-				List<RevCommit> commits = JGitUtils.getRevLog(repo, temp, cal.getTime());
-				for (RevCommit commit : commits) {
-					count += 1;
-				}
-				output += "Branch Name: " + branch + ", No of Branch:" + number + ", No of Commit:" + count + "<br>";
+  }
 
-			}
-		}
+  @GET
+  @Path("/activities")
+  public Response getActivities() {
+    List<ActivityModel> activities = dashBoardService.populateActivities(true, -1);
+    String str = "";
+    for (ActivityModel activity : activities) {
+      str += activity.toString();
+    }
+    return Response.status(200).entity(str).build();
+  }
 
-		return Response.status(200).entity(output).build();
+  @GET
+  @Path("/activitiesInJson")
+  public Response getActivitiesInJson(@DefaultValue("true") @QueryParam("cached") boolean cached,
+      @DefaultValue("-1") @QueryParam("daysBack") int daysBack) {
 
-	}
+    List<ActivityModel> activities = dashBoardService.populateActivities(cached, daysBack);
+    String jsonOutput = JacksonUtils.convertActivitiestoJson(activities);
+    String finalOutput = "{ \"data\":" + jsonOutput + "}";
+    return Response.status(200).entity(finalOutput).header("Access-Control-Allow-Origin", "*")
+        .build();
+  }
 
-	@GET
-	@Path("/{repository}/tickets/{ticketid}")
-	public Response getTickets(@PathParam("repository") String repoName, @PathParam("ticketid") String ticket) {
-
-		List<ExtCommitModel> commits = commitService.getDetailsForBranchName(ticket);
-		String jsonOutput = JacksonUtils.toTicketCommitsJson(commits);
-		String finalOutput = "{ \"data\":" + jsonOutput + "}";
-
-		return Response.status(200).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
-	}
-
-	@GET
-	@Path("/activities")
-	public Response getActivities() {
-		List<ActivityModel> activities = dashBoardService.populateActivities(true, -1);
-		String str = "";
-		for (ActivityModel activity : activities) {
-			str += activity.toString();
-		}
-		return Response.status(200).entity(str).build();
-	}
-
-	@GET
-	@Path("/activitiesInJson")
-	public Response getActivitiesInJson(@DefaultValue("true") @QueryParam("cached") boolean cached,
-			@DefaultValue("-1") @QueryParam("daysBack") int daysBack) {
-
-		List<ActivityModel> activities = dashBoardService.populateActivities(cached, daysBack);
-		String jsonOutput = JacksonUtils.toJson(activities);
-		String finalOutput = "{ \"data\":" + jsonOutput + "}";
-		return Response.status(200).entity(finalOutput).header("Access-Control-Allow-Origin", "*").build();
-	}
-
-	protected Repository getRepository(String repositoryName) {
-		Repository r = getRepositoryManager().getRepository(repositoryName);
-		if (r == null) {
-			System.out.println("\n\nCannot Load Repository" + " " + repositoryName);
-			return null;
-		}
-		return r;
-	}
-
+  @GET
+  @Path("/appPath")
+  public Response applicationPaths() {
+    String dir = ApplicationDirectoryUtils.getProgramDirectory();
+    return Response.status(200).entity("Application Dir: " + dir)
+        .header("Access-Control-Allow-Origin", "*").build();
+  }
 }
